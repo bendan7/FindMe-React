a273b8e99700e56ba5c5aa7621e7e25dceb55fac
Ben Dan <bendan7@gmail.com>
2021-02-04 10:43:57 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [ip, setIp] = useState("0.0.0.0");
+  const [geolocation, setGeolocation] = useState(null);
+  const [address, setAddress] = useState({ city: "", country: "" });
+
+  useEffect(() => {
+    fetch(
+      `https://api.ipdata.co/?api-key=${process.env.REACT_APP_IPDATA_API_KEY}`
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        setIp(json["ip"]);
+        setAddress({ country: json["country_name"], city: json["city"] });
+        setGeolocation({ lad: json["latitude"], long: json["longitude"] });
+      });
+  }, []);
+
+  const mapIframe =
+    geolocation != null ? (
+      <iframe
+        title="GoogleMaps"
+        src={`https://www.google.com/maps/embed/v1/place?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&q=${geolocation.lad},${geolocation.long}`}
+      />
+    ) : null;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} className="App-logo" alt="logo" />
+      <div>
+        <h1>{ip}</h1>
+        <h2>{address.country}</h2>
+        <h4>{address.city}</h4>
+      </div>
+      {mapIframe}
+      <a
+        className="App-link"
+        href="https://docs.google.com/document/d/1Qv61yNAizOfMb-WAPtR2NnkVBZreR6HiMXA4PgscwOM/edit?usp=sharing"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Ben - Resume
+      </a>
     </div>
   );
 }
